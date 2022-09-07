26ad37937ed1f10afcf151a1f4ddf3de827755ed
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-09-07 15:40:21 +0430

    Update LargeNumber.py

--- LargeNumber.py
+++ LargeNumber.py
@@ -15,569 +15,158 @@
 
 
 import numpy as np
-from tqdm import tqdm
+from LargeNumberFormat import LargeNumberFormat
+from utils import gcd, _add, string_int_to_very_long_int, convert_repeating_dec_to_frac
+from utils import dec_to_frac, number_to_simplest_form, to_fraction, _multiply
+from utils import _divide, _invert, _frac_to_dec
 
-def simple_gcd(x, y):
-    if x < y:
-        x, y = y, x
+
+largeNumberFormat = LargeNumberFormat()
+
+class LargeNumber(object):
+    def __init__(self, value):
+        self.value = number_to_simplest_form(str(value).replace('L', '').strip())
+
+    def __repr__(self):
+        return self.value
     
-    while(y != 0):
-        remainder = x % y
-        x = y
-        y = remainder
+    def __str__(self):
+        return self.value
+
+    def __len__(self):
+        return len(self.value)
+
+    def __add__(self, x):
+        if type(x) == LargeNumber:
+            sum = _add(self.value, x.value)
+        elif type(x) == int or str:
+            sum = _add(self.value, str(x))
+        else:
+            raise ValueError('The data type {} is not supported!'.format(type(x)))
+
+        if ('/' in sum) and (not largeNumberFormat.return_fracation):
+           sum = _frac_to_dec(sum, repeating_form=largeNumberFormat.return_repeating_form, max_dec_num=largeNumberFormat.precision)
+        return LargeNumber(sum)
+
+    def __sub__(self, x):
+        if '-' in x.value:
+            return self.__add__(x.value.replace('-', ''))
+        elif '+' in x.value:
+            return self.__add__('-' + x.value.replace('+', ''))
+        else:
+            return self.__add__('-' + x.value)
+
+
+    def __mul__(self, x):
+        if type(x) == LargeNumber:
+            return LargeNumber(_multiply(self.value,  x.value))
+        elif type(x) == int or str:
+            return LargeNumber(_multiply(self.value,  str(x)))
+        else:
+            raise ValueError('The data type {} is not supported!'.format(type(x)))
+
+    def __truediv__(self, x):
+        if type(x) == LargeNumber:
+            return LargeNumber(_divide(self.value,
+                                       x.value,
+                                       max_dec_num = largeNumberFormat.precision,
+                                       repeating_form = largeNumberFormat.return_repeating_form,
+                                       return_frac = largeNumberFormat.return_fracation))
+        elif type(x) == int or str:
+            return LargeNumber(_divide(self.value,
+                                       str(x),
+                                       max_dec_num = largeNumberFormat.precision,
+                                       repeating_form = largeNumberFormat.return_repeating_form,
+                                       return_frac = largeNumberFormat.return_fracation))
+        else:
+            raise ValueError('The data type {} is not supported!'.format(type(x)))
     
-    return x
-
-
-def gcdExtended(a, b):
-    if a == 0 : 
-        return b, 0, 1
+    def __floordiv__(self, x):
+        if type(x) == LargeNumber:
+            return LargeNumber(_divide(self.value,
+                                       x.value,
+                                       max_dec_num = 1,
+                                       repeating_form = False,
+                                       return_frac = False).split('.')[0].replace('L',''))
+        elif type(x) == int or str:
+            return LargeNumber(_divide(self.value,
+                                       str(x),
+                                       max_dec_num = 1,
+                                       repeating_form = False,
+                                       return_frac = False).split('.')[0].replace('L',''))
+        else:
+            raise ValueError('The data type {} is not supported!'.format(type(x)))
             
-    gcd, x1, y1 = gcdExtended(b%a, a)
-    x = y1 - (b//a) * x1
-    y = x1
-    
-    return gcd, x, y
-
-
-def gcd(x, y, _type='simple'):
-    if _type == 'simple':
-        return simple_gcd(x, y)
-    else:
-        gcd,_,_, = gcdExtended(x, y)
-        return gcd
-
-
-def string_int_to_very_long_int(input):
-    x = np.array(list(input), dtype='int').astype('object')
-    i_s = np.arange(len(x))[::-1].astype('object')
-    out = np.array([np.sum((10**i_s)*x)], dtype='object')
-    return out
-
-
-def convert_repeating_dec_to_frac(input, gcd_type='extended'):
-    input_sign = '-' if input[0] == '-' else ''
-    input = input[1:] if input[0] == '-' or input[0] == '+' else input 
-
-    int_dec = input.split('.')
-    I = int_dec[0]
-
-    fixed_repeating = int_dec[1].split('R')
-    A = fixed_repeating[0]
-    P = fixed_repeating[1]
-
-    numerator = string_int_to_very_long_int(I+A+P) - string_int_to_very_long_int(I+A)
-    denominator = string_int_to_very_long_int('9'*len(P) + '0'*len(A))
-    
-
-    gcd_val = gcd(numerator, denominator, _type=gcd_type)
-
-    numerator = numerator // gcd_val
-    denominator =  denominator // gcd_val
-
-    fraction = input_sign + str(numerator[0]) + '/' + str(denominator[0])
-
-    return fraction
-
-
-def dec_to_frac(input):
-    input_sign = '-' if input[0] == '-' else ''
-    input = input[1:] if input[0] == '-' or input[0] == '+' else input 
-
-    dec_len = len(input.split('.')[1])
-    input = input.replace('.' , '')
-
-    out = number_to_simplest_form(input) + '/' + '1' + '0'*dec_len
-    return input_sign + out
-    
-
-def number_to_simplest_form(input):
-    if type(input) is not str:
-        input = str(input)
-
-    input_sign = '-' if input[0] == '-' else ''
-    input = input[1:] if input[0] == '-' or input[0] == '+' else input 
-	
-    if '/' in input:
-        parts = input.split('/')
-        s1 = number_to_simplest_form(parts[0])
-        s2 = number_to_simplest_form(parts[1])
-        if s2 == '1':
-            input = s1
+    def __eq__(self, x):
+        if type(x) == LargeNumber:
+            a, b = to_fraction(x.value)
+            a_sign = -1 if '-' in a else 1
+            a = a.replace('-', '').replace('+', '')
+        elif type(x) == int or str:
+            a, b = to_fraction(str(x))
         else:
-	        input = s1 + '/' + s2
+            raise ValueError('The data type {} is not supported!'.format(type(x)))
+        c, d = to_fraction(self.value)
+        c_sign = -1 if '-' in c else 1
+        c = c.replace('-', '').replace('+', '')
 
 
+        a = string_int_to_very_long_int(a)
+        b = string_int_to_very_long_int(b)
+        c = string_int_to_very_long_int(c)
+        d = string_int_to_very_long_int(d)
 
-    if '.' in input:
-        parts = input.split('.')
-        if 'R' in parts[1]:
-            R_index = list(parts[1]).index('R')
-            part1_set = set(parts[1][R_index:])
-            if len(part1_set) == 2:
-                if '0' in part1_set and 'R' in part1_set:
-                    return input_sign + number_to_simplest_form(parts[0]+ '.' + parts[1][:R_index])
-        if len(parts[1]) == 0:
-            return input_sign + number_to_simplest_form(input[:-1])
-        if len(parts[0]) == 0:
-            return input_sign + number_to_simplest_form('0' + input)
-        if parts[1][-1] == '0' and 'R' not in parts[1]:
-            return  input_sign + number_to_simplest_form(input[:-1])
-    if  len(input)>=2:
-        if input[0] == '0' and input[1] != '.':
-            return  input_sign + number_to_simplest_form(input[1:])
-    return input_sign + input
+        g1 = gcd(a, b)
+        a = a//g1
+        b = b//g1
 
+        g2 = gcd(c, d)
+        c = c//g2
+        d = d//g2
 
-
-def _sign(x):
-    if '-' in x:
-        return -1
-    if '-' not in x:
-        return 1
-
-
-
-def to_fraction(input):
-    if 'R' in input:
-        return to_fraction(convert_repeating_dec_to_frac(input))
-    if 'R' not in input and '.' in input:
-        return to_fraction(dec_to_frac(input))
-    if '/' in input:
-        return input.split('/')
-    return input, '1'
-
-
-def _greater(x, y):  # x > y
-    x = number_to_simplest_form(x)
-    y = number_to_simplest_form(y)
-
-    flag1 = False
-    flag2 = False
-
-    if '-' in x:
-        flag1 = True
-        x = x.replace('-', '0')
-    else:
-        x = x.replace('+', '1') if '+' in x else '1' + x
-
-    if '-' in y:
-        flag2 = True
-        y = y.replace('-', '0')
-    else:
-        y = y.replace('+', '1') if '+' in x else '1' + y
-
-    if flag1 and flag2:
-        x, y = y, x
-
-    if '.' in x:
-        x_dec_len = len(x.split('.')[1])
-    else:
-        x_dec_len = 0
-    if '.' in y:
-        y_dec_len = len(y.split('.')[1])
-    else:
-        y_dec_len = 0
-
-    x = x.replace('.', '')
-    y = y.replace('.', '')
-    
-    if x_dec_len > y_dec_len:
-        y = y + '0'*(x_dec_len - y_dec_len)
-        y_dec_len = x_dec_len
-    elif x_dec_len < y_dec_len:
-        x = x + '0'*(y_dec_len - x_dec_len)
-        x_dec_len = y_dec_len
-
-    x_len = len(x)
-    y_len = len(y)
-
-    if x_len > y_len:
-        y = '0'*(x_len - y_len) + y
-    elif x_len < y_len:
-        x = '0'*(y_len - x_len) + x
-
-    # method1
-    '''x = np.array([int(i) for i in x])
-    y = np.array([int(i) for i in y])
-
-    t = (x == y)
-    index = np.where(t == False)
-
-    if len(index) == 0:
-        return x[0] > y[0]
-
-    x = x[index]
-    y = y[index]
-
-    return x[0] > y[0]'''
-
-    # method2
-    x = string_int_to_very_long_int(x)
-    y = string_int_to_very_long_int(y)
-    
-
-    return x > y
+        return (a_sign*a == c_sign*c and b == d)[0]
 
     
-
-
-def _add(x, y):
-    if ('R' in x) or ('R' in y) or ('/' in x) or ('/' in y):
-        sum = _add_frac(x, y)
-        return sum
-
-
-    if type(x) is str:
-        x_sign = -1 if x[0] == '-' else 1
-    else:
-        x_sign = 1 if x > 0 else -1
-    if type(y) is str:
-        y_sign = -1 if y[0] == '-' else 1
-    else:
-        y_sign = 1 if y > 0 else -1
-
-    x = x.replace('-', '').replace('+', '')
-    y = y.replace('-', '').replace('+', '')
-
-    x = number_to_simplest_form(x)
-    y = number_to_simplest_form(y)
-
-    if '.' in x:
-        x_dec_len = len(x.split('.')[1])
-    else:
-        x_dec_len = 0
-    if '.' in y:
-        y_dec_len = len(y.split('.')[1])
-    else:
-        y_dec_len = 0
-        
-
-    x = x.replace('.', '')
-    y = y.replace('.', '')
-    
-    if x_dec_len > y_dec_len:
-        y = y + '0'*(x_dec_len - y_dec_len)
-        y_dec_len = x_dec_len
-    elif x_dec_len < y_dec_len:
-        x = x + '0'*(y_dec_len - x_dec_len)
-        x_dec_len = y_dec_len
-
-    x_len = len(x)
-    y_len = len(y)
-
-    if x_len > y_len:
-        y = '0'*(x_len - y_len) + y
-    elif x_len < y_len:
-        x = '0'*(y_len - x_len) + x
-
-    
-    # method1
-    '''carry = 0
-    out = []
-    for (i, j) in zip(x[::-1], y[::-1]):
-        sum = int(i) + int(j) + carry
-        out.append(str(sum%10))
-        carry = sum//10
-
-    if carry > 0:
-        out = str(carry) + ''.join(out[::-1])
-    else:
-        out = ''.join(out[::-1])
-
-    if x_dec_len > 0 or y_dec_len > 0:
-        out = out[:-x_dec_len] +'.' +out[-x_dec_len:]
-    
-    return out'''
-
-
-    # method2
-    '''x = np.array([int(i) for i in x])
-    y = np.array([int(i) for i in y])
-
-    sum =  x + y
-    carry = sum//10
-
-    carry = np.concatenate([carry[1:], np.array([0])]) # shift array
-    sum[1:] = sum[1:] % 10
-    sum = sum + carry
-
-    carry = sum//10
-    carry = np.concatenate([carry[1:], np.array([0])]) # shift array
-    sum[1:] = sum[1:] % 10
-    sum = sum + carry
-
-    sum = ''.join([str(i) for i in sum])'''
-
-    # method3
-    x = string_int_to_very_long_int(x)
-    y = string_int_to_very_long_int(y)
-
-    sum = str((x_sign * x + y_sign * y)[0])
-
-    
-    if x_sign*y_sign>0:
-        if x_dec_len > 0 or y_dec_len > 0:
-            sum = sum[:-x_dec_len] + '.' + sum[-x_dec_len:]
-    else:
-        sum_sign = '-' if sum[0] == '-' else ''
-        sum = sum.replace('-', '')
-        if len(sum) < x_dec_len:
-            sum = '0.' + ''.join(['0']*(x_dec_len - len(sum))) + sum
+    def __gt__(self, x):
+        if type(x) == LargeNumber:
+            a, b = to_fraction(x.value)
+            a_sign = -1 if '-' in a else 1
+            a = a.replace('-', '').replace('+', '')
+        elif type(x) == int or str:
+            a, b = to_fraction(str(x))
         else:
-            sum = sum[:-x_dec_len] + '.' + sum[-x_dec_len:]
+            raise ValueError('The data type {} is not supported!'.format(type(x)))
+        c, d = to_fraction(self.value)
+        c_sign = -1 if '-' in c else 1
+        c = c.replace('-', '').replace('+', '')
+
+
+        a = string_int_to_very_long_int(a)
+        b = string_int_to_very_long_int(b)
+        c = string_int_to_very_long_int(c)
+        d = string_int_to_very_long_int(d)
+
+        g1 = gcd(a, b)
+        a = a//g1
+        b = b//g1
+
+        g2 = gcd(c, d)
+        c = c//g2
+        d = d//g2
+
+        p1 = _multiply(str(a[0]), str(d[0]))
+        p2 = _multiply(str(c[0]), str(b[0]))
+
+        p1 = string_int_to_very_long_int(p1)
+        p2 = string_int_to_very_long_int(p2)
+
+        return (a_sign*p1 < c_sign*p2)[0]
+
+
+
+    def __ge__(self, x):
+        return self.__gt__(x) or self.__eq__(x)
+
+    
+    
         
-        sum = sum_sign + sum
-    
-    return number_to_simplest_form(sum)
-
-
-
-def _multiply(x, y):
-    if ('R' in x) or ('R' in y) or ('/' in x) or ('/' in y):
-        prod = _multiply_frac(x, y)
-        return prod
-
-    if type(x) is str:
-        x_sign = -1 if x[0] == '-' else 1
-    else:
-        x_sign = 1 if x > 0 else -1
-    if type(y) is str:
-        y_sign = -1 if y[0] == '-' else 1
-    else:
-        y_sign = 1 if y > 0 else -1
-
-    x = x.replace('-', '').replace('+', '')
-    y = y.replace('-', '').replace('+', '')
-
-    x = number_to_simplest_form(x)
-    y = number_to_simplest_form(y)
-
-    if '.' in x:
-        x_dec_len = len(x.split('.')[1])
-    else:
-        x_dec_len = 0
-    if '.' in y:
-        y_dec_len = len(y.split('.')[1])
-    else:
-        y_dec_len = 0
-
-    x = x.replace('.', '')
-    y = y.replace('.', '')
-    
-    if x_dec_len > y_dec_len:
-        y = y + '0'*(x_dec_len - y_dec_len)
-        y_dec_len = x_dec_len
-    elif x_dec_len < y_dec_len:
-        x = x + '0'*(y_dec_len - x_dec_len)
-        x_dec_len = y_dec_len
-
-    x_len = len(x)
-    y_len = len(y)
-
-    if x_len > y_len:
-        y = '0'*(x_len - y_len) + y
-    elif x_len < y_len:
-        x = '0'*(y_len - x_len) + x
-    
-
-    x = string_int_to_very_long_int(x)
-    y = string_int_to_very_long_int(y)
-
-
-    mul = str((x*y*x_sign*y_sign)[0])
-
-    if x_dec_len > 0 or y_dec_len > 0:
-        mul = mul[:-2*x_dec_len] + '.' + mul[-2*x_dec_len:]
-
-    return number_to_simplest_form(mul)
-
-
-
-
-
-def _divide(x, y, max_dec_num = 100, repeating_form = False, return_frac = False):  # x/y
-    if ('R' in x) or ('R' in y) or ('/' in x) or ('/' in y) or return_frac:
-        dev = _divide_frac(x, y)
-        a, b = to_fraction(dev)
-        if return_frac:
-            return a + '/' + b
-        
-        return _divide(a, b, max_dec_num = max_dec_num, repeating_form = repeating_form, return_frac = False)
-        
-
-    if type(x) is str:
-        x_sign = -1 if x[0] == '-' else 1
-    else:
-        x_sign = 1 if x > 0 else -1
-    if type(y) is str:
-        y_sign = -1 if y[0] == '-' else 1
-    else:
-        y_sign = 1 if y > 0 else -1
-
-    x = x.replace('-', '').replace('+', '')
-    y = y.replace('-', '').replace('+', '')
-
-    x = number_to_simplest_form(x)
-    y = number_to_simplest_form(y)
-
-    if '.' in x:
-        x_dec_len = len(x.split('.')[1])
-    else:
-        x_dec_len = 0
-    if '.' in y:
-        y_dec_len = len(y.split('.')[1])
-    else:
-        y_dec_len = 0
-
-    x = x.replace('.', '')
-    y = y.replace('.', '')
-    
-    if x_dec_len > y_dec_len:
-        y = y + '0'*(x_dec_len - y_dec_len)
-        y_dec_len = x_dec_len
-    elif x_dec_len < y_dec_len:
-        x = x + '0'*(y_dec_len - x_dec_len)
-        x_dec_len = y_dec_len
-
-    x_len = len(x)
-    y_len = len(y)
-
-    if x_len > y_len:
-        y = '0'*(x_len - y_len) + y
-    elif x_len < y_len:
-        x = '0'*(y_len - x_len) + x
-    
-
-    x = string_int_to_very_long_int(x)
-    y = string_int_to_very_long_int(y)
-
-    if y == 0:
-        raise ValueError('The denominator\'s value is zero')
-    
-    
-
-    int_part = str((x//y)[0]) + '.'
-    remainder = x % y
-    x = remainder * 10
-
-    dec_part = []
-    remainders = []
-    remainders_set = set()
-    counter = 0
-    index = -1
-
-    while(remainder[0] != 0 and counter < max_dec_num):
-        counter += 1 
-        remainders += [remainder]
-        remainders_set.add(remainder[0])
-        dec_part += [str((x//y)[0])]
-        remainder = x % y
-        x = remainder * 10
-
-        if repeating_form:
-            if remainder[0] in remainders_set:
-                counter = max_dec_num
-                index = remainders.index(remainder)
-
-
-    if index >= 0:
-        out = int_part + ''.join(dec_part[:index]) + 'R' + ''.join(dec_part[index:])
-    else:
-        out = int_part + ''.join(dec_part) 
-        out = number_to_simplest_form(out)
-        if remainder[0] != 0:
-            out += 'L'
-
-    out = '-' + out if x_sign*y_sign == -1 else out
-
-    return out
-        
-
-
-
-def _invert(x, return_frac = True, repeating_form = False, max_dec_num = 100):  # 1/x
-    return _divide('1', x, max_dec_num = max_dec_num, repeating_form = repeating_form, return_frac = return_frac)
-
-
-def _frac_to_dec(x, repeating_form = False, max_dec_num = 100):  # 1/x
-    a, b = to_fraction(x)
-    return _divide(a, b, max_dec_num = max_dec_num, repeating_form = repeating_form, return_frac = False)
-    
-def _add_frac(x, y):
-    a, b = to_fraction(x)   # x = a/b
-    c, d = to_fraction(y)   # y = c/d
-
-    t1 = _multiply(a,d)
-    t2 = _multiply(b,c)
-    t3 = _multiply(b,d)
-
-    t4 = _add(t1, t2)
-
-    t3_sign = -1 if t3[0] == '-' else 1
-    t4_sign = -1 if t4[0] == '-' else 1
-
-    t3 = t3.replace('-', '').replace('+', '')
-    t4 = t4.replace('-', '').replace('+', '')
-
-    t3 = string_int_to_very_long_int(t3)
-    t4 = string_int_to_very_long_int(t4)
-
-
-    g = gcd(t3, t4)
-
-    t3 = t3//g
-    t4 = t4//g
-
-    t4 *= t3_sign*t4_sign
-    
-    if t3 != 1: 
-        num = number_to_simplest_form(str(t4[0]))
-        den = number_to_simplest_form(str(t3[0]))
-        return num + '/' + den
-    else:
-        num = number_to_simplest_form(str(t4[0]))
-        return num
-
-
-def _multiply_frac(x, y):
-    a, b = to_fraction(x)   # x = a/b
-    c, d = to_fraction(y)   # y = c/d
-
-    t1 = _multiply(a,c)
-    t2 = _multiply(b,d)
-
-    t1_sign = -1 if t1[0] == '-' else 1
-    t2_sign = -1 if t2[0] == '-' else 1
-
-    t1 = t1.replace('-', '').replace('+', '')
-    t2 = t2.replace('-', '').replace('+', '')
-
-    t1 = string_int_to_very_long_int(t1)
-    t2 = string_int_to_very_long_int(t2)
-
-    g = gcd(t1, t2)
-
-    t1 = t1//g
-    t2 = t2//g
-
-    t1 *= t1_sign*t2_sign
-
-    if t2 != 1: 
-        num = number_to_simplest_form(str(t1[0]))
-        den = number_to_simplest_form(str(t2[0]))
-        return num + '/' + den
-    else:
-        num = number_to_simplest_form(str(t1[0]))
-        return num
-
-
-
-def _divide_frac(x, y):
-    y_sign = '-' if y[0] == '-' else ''
-    y = y.replace('-', '').replace('+', '')
-    c, d = to_fraction(y)   # y = c/d
-
-    return _multiply_frac(x, y_sign + d +'/'+c)
-
-
