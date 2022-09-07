8de4e0dd6409d7a0c16185065e658031fd2341f9
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-09-07 23:05:51 +0430

    Fix some bugs

--- LargeNumber.py
+++ LargeNumber.py
@@ -28,6 +28,7 @@ class LargeNumber(object):
         self.value = number_to_simplest_form(str(value).replace('L', '').replace('+', '').strip())
         self.larger_than_precision = True if 'L' in str(value) else False
 
+
     def __repr__(self):
         if self.larger_than_precision:
             return self.value + 'L'
