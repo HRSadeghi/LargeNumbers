db5c8b2f6e51382b38786349ba05d7229004c0ad
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-09-08 12:31:57 +0430

    Change the project structure

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     license='Apache License 2.0',
     packages=['LargeNumbers'],
     install_requires=['numpy'],
+    long_description = long_description,
 
     classifiers=[
         'License :: OSI Approved :: BSD License',  
